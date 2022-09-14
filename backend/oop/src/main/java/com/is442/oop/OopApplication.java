19cd941722eb6ed567a7d91fe7d25a4451b00ee0
Chew Yi Xin <chewyixin1999@gmail.com>
2022-09-14 18:42:02 +0800

    add springboot app

---
+++ backend/oop/src/main/java/com/is442/oop/OopApplication.java
@@ -0,0 +1,13 @@
+package com.is442.oop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopApplication.class, args);
+	}
+
+}
---
+++ backend/oop/src/test/java/com/is442/oop/OopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.is442.oop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
