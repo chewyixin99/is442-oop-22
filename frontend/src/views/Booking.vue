5e71c891a43714a1d448fce99168f12857fa43bc
GanJL <jianlin.gan.2020@scis.smu.edu.sg>
2022-09-18 01:27:12 +0800

    FE-1
    - Implemented basic routing
    - Included details of components and functionalities needed

---
+++ frontend/src/views/Booking.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="">
+        <h1>Booking</h1>
+        ------------------------------------
+        <p>Staff functionalities:</p>
+        ------------------------------------
+        <p>1. View current and past bookings</p>
+        <p>2. Create new bookings</p>
+        <p>3. Edit/cancel current booking</p>
+        <p>4. View number of bookings count left</p>
+        <p>5. View contact info of previous booker</p>
+        <p>6. View physical pass status: Issued/Available/Unavailable</p>
+        ------------------------------------
+        <p>Admin functionalities:</p>
+        ------------------------------------
+        <p>1. View current and pass bookings</p>
+        <p>2. Create new bookings</p>
+        <p>3. Edit/cancel current booking</p>
+        <p>4. View number of bookings count left</p>
+        <p>5. View contact info of previous booker</p>
+        <p>6. View current and past staff booking - booking log</p>
+        <p>7. Create new booking for staff</p>
+        <p>8. Edit/cancel current booking for staff</p>
+        <p>9. View number of bookings count left for staff</p>
+        <p>10. View physical pass status: Issued/Available/Unavailable</p>
+        <p>11. Edit physical pass status: Issued/Available/Unavailable</p>
+        ------------------------------------
+        <p>GOP functionalities:</p>
+        ------------------------------------
+        <p>1. View current and pass staff booking - booking log</p>
+        <p>2. Edit physical pass status: Issued/Available/Unavailable</p>    
+        
+        </div>
+</template>
---
+++ frontend/src/views/Email.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="">
+        <h1>Email</h1>
+        ------------------------------------
+        <p>Admin functionalities:</p>
+        ------------------------------------
+        <p>1. Edit email template for the two types of email</p>
+        <p>2. Settings for automated email</p>
+    </div>
+</template>
