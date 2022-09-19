2714feecdcac9e1dab01f6a3bb4124d8f7aaedbf
yy <yinyao.lim.2020@scis.smu.edu.sg>
2022-09-20 02:16:02 +0800

    FE-3
    - Workaround on sidebar overlapping with main content

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 
+
 createApp(App)
     .use(router)
     .mount('#app')
--- frontend/src/views/Pass.vue
+++ frontend/src/views/Pass.vue
@@ -12,4 +12,43 @@
         <p>2. Create new passes</p>
         <p>3. Edit/delete passes</p>
     </div>
+    <div class=" d-flex justify-content-center col-sm-10 float-end">
+        <div class="col">
+            <div class="card">
+            <img src="" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+            <img src="" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+            <img src="" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+            <img src="" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            </div>
+        </div>
+    </div>
+    
 </template>
