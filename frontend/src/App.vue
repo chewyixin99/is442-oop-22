93f5a90edc62472a1ff2032dfe310a5141efafc0
yy <yinyao.lim.2020@scis.smu.edu.sg>
2022-09-20 23:31:38 +0800

    FE-4
    -VueQuill Text editor for email

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -14,6 +14,7 @@ export default {
   name: 'App',
   components: {
     Sidebar
+    
   }
 }
 </script>
--- frontend/src/views/Email.vue
+++ frontend/src/views/Email.vue
@@ -1,10 +1,25 @@
 <template>
-    <div class="">
+    <div class="col-sm-10 float-end">
         <h1>Email</h1>
         ------------------------------------
         <p>Admin functionalities:</p>
         ------------------------------------
         <p>1. Edit email template for the two types of email</p>
         <p>2. Settings for automated email</p>
+        <QuillEditor theme="snow" />
     </div>
+
 </template>
+
+<script>
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+
+export default {
+  components: {
+    QuillEditor
+  }
+}
+
+</script>
+
