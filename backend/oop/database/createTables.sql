877904229aa60399ba8bbb0588a219f0eafef3b4
Chew Yi Xin <chewyixin1999@gmail.com>
2022-10-04 14:17:22 +0800

    refactor sql script

--- backend/oop/database/createTables.sql
+++ backend/oop/database/createTables.sql
@@ -4,7 +4,7 @@ use `oop`;
 
 -- This is done, and follows the ER diagram in the Google Drive
 -- Link here: https://app.diagrams.net/#G1TDR1A4IQ3V2OcwaAHy8hvXOS3MokRVjd
-create table if not exists `oop`.`email_template` (
+create table if not exists `email_template` (
     `tid` int auto_increment not null,
     `template` varchar(1000) not null,
     `defunct` bit not null, -- 1 for True and 0 for False
@@ -18,7 +18,7 @@ INSERT INTO `email_template` (`tid`, `template`, `defunct`) VALUES
 (4, 'template4', 0);
 COMMIT;
 
-create table if not exists `oop`.`user` (
+create table if not exists `user` (
     `uid` int auto_increment not null,
     `username` varchar(50) not null,
     `password` varchar(50) not null, 
@@ -36,7 +36,7 @@ INSERT INTO `user` (`uid`, `username`, `password`, `email`, `contact_number`, `u
 (4, 'jianlin', 'jianlinpw', 'jianlin@mail.com', '91234567', 'GOP', 0);
 COMMIT;
 
-create table if not exists `oop`.`passes` (
+create table if not exists `pass` (
   `pid` int auto_increment not null,
   `tid` int,
   `num_guests` int not null,
@@ -45,31 +45,31 @@ create table if not exists `oop`.`passes` (
   `is_physical` bit not null,
   `defunct` bit not null, -- 1 for True and 0 for False
   primary key (`pid`),
-  foreign key (`tid`) references `oop`.`email_template` (`tid`)
+  foreign key (`tid`) references `email_template` (`tid`)
 ) engine=InnoDB default charset=utf8;
 
-INSERT INTO `passes` (`pid`, `tid`, `num_guests`, `replacement_fee`, `poi`, `is_physical`, `defunct`) VALUES
+INSERT INTO `pass` (`pid`, `tid`, `num_guests`, `replacement_fee`, `poi`, `is_physical`, `defunct`) VALUES
 (1, 1, 2, 20.00, 'Gardens By The Bay', 1, 0),
 (2, 2, 2, 20.00, 'Singapore Zoo', 1, 1),
 (3, 3, 2, 20.00, 'Sea Aquarium', 0, 0),
 (4, 4, 2, 20.00, 'Sea Aquarium', 0, 0);
 COMMIT;
 
-create table if not exists `oop`.`loans` (
+create table if not exists `loan` (
     `lid` int auto_increment not null,
     `uid` int not null,
     `gid` int, 
     `pid` int not null,
     `is_completed` bit not null, 
-    `date_start` varchar(50),
-    `date_end` varchar(50),
+    `start_date` varchar(50),
+    `end_date` varchar(50),
     primary key (`lid`,`uid`,`pid`),
-    foreign key (`uid`) references `oop`.`User` (`uid`),
-    foreign key (`gid`) references `oop`.`User` (`uid`),
-    foreign key (`pid`) references `oop`.`Passes` (`pid`)
+    foreign key (`uid`) references `user` (`uid`),
+    foreign key (`gid`) references `user` (`uid`),
+    foreign key (`pid`) references `pass` (`pid`)
 ) engine = InnoDB default charset = utf8;
 
-INSERT INTO `loans` (`lid`, `uid`, `gid`, `pid`, `is_completed`, `date_start`, `date_end`) VALUES
+INSERT INTO `loan` (`lid`, `uid`, `gid`, `pid`, `is_completed`, `start_date`, `end_date`) VALUES
 (1, 1, 4, 1, 1, '1/10/2022', '2/10/2022'),
 (2, 2, 4, 2, 1, '3/10/2022', '4/10/2022'),
 (3, 3, 4, 3, 0, '5/10/2022', '6/10/2022'),
