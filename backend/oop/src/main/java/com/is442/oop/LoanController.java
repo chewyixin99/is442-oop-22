eb19ea38ae2c983543da3c2530f0ff987669e7ae
Shaan Walia <mxixqc@gmail.com>
2022-10-10 16:09:03 +0800

    completed getLoanByLoanID

--- backend/oop/src/main/java/com/is442/oop/LoanController.java
+++ backend/oop/src/main/java/com/is442/oop/LoanController.java
@@ -4,10 +4,12 @@ package com.is442.oop;
 import com.is442.oop.daos.LoanDAO;
 import com.is442.oop.daos.LoanDAOInt;
 import com.is442.oop.data.models.Loan;
+import com.is442.oop.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,8 +27,17 @@ public class LoanController {
         System.out.println(loans);
         return new ResponseEntity<>(loans, HttpStatus.OK);
     }
-
-
+    // can be improved to give a more verbose error
+    @GetMapping("/{loanId}")
+    public ResponseEntity<Loan> getLoanByLoanID(@PathVariable("loanId") Integer loanId) {
+        Loan loan = null;
+        try{
+            loan = loanDAOInt.getLoanByLoanID(loanId);
+        } catch (Exception e){
+            return new ResponseEntity<>(loan, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(loan, HttpStatus.OK);
+    }
 
 }
 
