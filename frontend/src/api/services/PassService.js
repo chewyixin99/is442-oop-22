e2d5c0f80aac696a46126a7c497fc3eefb6f1f46
yy <yinyao.lim.2020@scis.smu.edu.sg>
2022-10-17 03:24:52 +0800

    FE-39
    -Pass basic CRUD function complete

--- frontend/src/api/services/PassService.js
+++ frontend/src/api/services/PassService.js
@@ -12,16 +12,26 @@ class PassService extends BaseApiService {
             return this.handleError(error);
         }
     }
-    async createPass(passDetails) {
-        try {
-            let pass = await axiosClient.post("/passes", { ...passDetails });
-            console.log(pass);
-            return pass.data
 
-        } catch (error) {
-            return this.handleError(error);
-        }
-    }
+    // async createPass(passDetails) {
+    //     try {
+    //         let pass = await axiosClient.post("/passes", { ...passDetails });
+    //         console.log(pass);
+    //         return pass.data
+
+    //     } catch (error) {
+    //         return this.handleError(error);
+    //     }
+    // }
+    // async deletePass(passID) {
+    //     try {
+    //         let deletePass = await axiosClient.delete("/passes/", { ...passID })
+    //         console.log(deletePass)
+    //         return deletePass.data.message
+    //     } catch (error) {
+    //         return this.handleError(error);
+    //     }
+    // }
 }
 
 export default new PassService();
