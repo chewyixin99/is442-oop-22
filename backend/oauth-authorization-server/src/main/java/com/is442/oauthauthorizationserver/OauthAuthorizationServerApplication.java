ffeafbfca7f0f4327777f15ce855caa5ca73de0f
Chew Yi Xin <chewyixin1999@gmail.com>
2022-10-25 01:41:37 +0800

    auth server set up

---
+++ backend/oauth-authorization-server/src/main/java/com/is442/oauthauthorizationserver/OauthAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com.is442.oauthauthorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthAuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ backend/oauth-authorization-server/src/main/java/com/is442/oauthauthorizationserver/config/DefaultSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.is442.oauthauthorizationserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.is442.oauthauthorizationserver.auth.CustomAuthenticationProvider;
+
+@EnableWebSecurity
+public class DefaultSecurityConfig {
+
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Autowired
+    public void bindAuthenticationProvider(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        authenticationManagerBuilder
+                .authenticationProvider(customAuthenticationProvider);
+    }
+}
