3bd883dae3532e1c1fb818af5ded35655e22b7da
Chew Yi Xin <chewyixin1999@gmail.com>
2022-10-26 18:28:17 +0800

    jwt api guard up, not sure if need to hide PRK PUK and comment out cors in main

--- backend/oop/src/main/java/com/is442/oop/config/WebSecurityConfig.java
+++ backend/oop/src/main/java/com/is442/oop/config/WebSecurityConfig.java
@@ -1,21 +1,45 @@
 package com.is442.oop.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    private static final String[] WHITE_LIST_URLS = {
-        "/hello",
-        "/register",
-        "/verifyRegistration*",
-        "/resendVerificationToken*"
-    };
+    private final RsaKeyProperties rsaKeys;
+
+    // private static final String[] WHITE_LIST_URLS = {
+    //     "/token",
+    //     "/register",
+    //     "/verifyRegistration*",
+    //     "/resendVerificationToken*"
+    // };
+
+    public WebSecurityConfig(RsaKeyProperties rsaKeys) {
+        this.rsaKeys = rsaKeys;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -23,17 +47,54 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public InMemoryUserDetailsManager user() {
+        return new InMemoryUserDetailsManager(
+            User.withUsername("admin")
+                .password(passwordEncoder().encode("adminpw"))
+                .authorities("read")
+                .build()
+        );
+    }
+
+    // @Bean
+    // SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    //     http
+    //         .cors()
+    //         .and()
+    //         .csrf()
+    //         .disable()
+    //         .authorizeHttpRequests()
+    //         .antMatchers(WHITE_LIST_URLS)
+    //         .permitAll();
+
+    //     return http.build();
+    // }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .cors()
             .and()
             .csrf()
             .disable()
-            .authorizeHttpRequests()
-            .antMatchers(WHITE_LIST_URLS)
-            .permitAll();
-
+            .authorizeRequests(auth -> auth.anyRequest().authenticated())
+            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .httpBasic(Customizer.withDefaults());
+            
         return http.build();
+    } 
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeys.publicKey()).build();
+    }
+    
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(rsaKeys.publicKey()).privateKey(rsaKeys.privateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
     }
 
 }
---
+++ backend/oop/src/main/java/com/is442/oop/jwttoken/JwtTokenService.java
@@ -0,0 +1,39 @@
+package com.is442.oop.jwttoken;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtTokenService {
+    
+    private final JwtEncoder jwtEncoder;
+
+    public JwtTokenService(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
+    }
+
+    public String generateToken(Authentication authentication) {
+        Instant now = Instant.now(); // timestamp
+        String scope = authentication.getAuthorities().stream()
+            .map(GrantedAuthority::getAuthority)
+            .collect(Collectors.joining(" "));
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+            .issuer("self") // self signing JWTs
+            .issuedAt(now)
+            .expiresAt(now.plus(1, ChronoUnit.HOURS)) // expires in an hour
+            .subject(authentication.getName()) // princiapl / subject
+            .claim("scope", scope) // scope defined above
+            .build();
+
+        return this.jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+}
